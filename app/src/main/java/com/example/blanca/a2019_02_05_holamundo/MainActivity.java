15aae163ce1e14298915982fe67a58e832300602
unknown <blancavicenibs@gmail.com>
2019-02-08 21:04:06 +0100

    he creado el proyecto

---
+++ app/src/main/java/com/example/blanca/a2019_02_05_holamundo/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.blanca.a2019_02_05_holamundo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
