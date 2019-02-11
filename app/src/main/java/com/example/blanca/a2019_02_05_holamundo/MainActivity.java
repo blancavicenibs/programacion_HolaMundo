5d647a74ab81640c7e6c3a0c50a2cf8e864ce07c
unknown <blancavicenibs@gmail.com>
2019-02-11 20:51:17 +0100

    he hecho el log

--- app/src/main/java/com/example/blanca/a2019_02_05_holamundo/MainActivity.java
+++ app/src/main/java/com/example/blanca/a2019_02_05_holamundo/MainActivity.java
@@ -2,12 +2,50 @@ package com.example.blanca.a2019_02_05_holamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button2;
+    EditText editText;
+    TextView textView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
+        button2 = (Button) findViewById(R.id.button2);
+        textView = (TextView) findViewById(R.id.textView);
+        editText = (EditText) findViewById(R.id.editText);
+
+
+
+        Log.d( "prueba",  "estoy en onCreate()" ); //debug
+
+
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                String resultado = editText.getText().toString();
+
+                textView.setText(String.valueOf(resultado.length()));
+
+            }
+        });
+
+
+
     }
 }
